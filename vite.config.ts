be3a4fb3caff4fafd0341702fad774211dc4d169
AHMED739268 <mo017886@gmail.com>
2025-08-29 14:59:21 +0300

    softwarehouse

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import svgr from "vite-plugin-svgr";
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-
+import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  // أهم جزء
-  preview: {
-    port: 4173,
-  },
-  // ده اللي بيظبط fallback
-  base: '/',
+
 })
