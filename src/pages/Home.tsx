59b121970081dd7333ba274446c85ed8d3f2f174
AHMED739268 <mo017886@gmail.com>
2025-08-29 17:58:28 +0300

    softwarehouse

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,33 +2,33 @@ import Hero from '../sections/Hero'
 import Partners from '../sections/Partners'
 
 import Features from '../pages/FeaturesPage'
-// import Features2 from '../pages/FeaturesPage2'
-// import StoreBuilder from '../sections/StoreBuilder'
-// import StoreBuilderRight from '../sections/StoreBuilderRight'
-// import SuccessStories from '../pages/SuccessStories'
-// import {SupportSection} from '../sections/SupportSection';
-// import { IconKey } from 'utils/iconMap'
+import Features2 from '../pages/FeaturesPage2'
+import StoreBuilder from '../sections/StoreBuilder'
+import StoreBuilderRight from '../sections/StoreBuilderRight'
+import SuccessStories from '../pages/SuccessStories'
+import {SupportSection} from '../sections/SupportSection';
+import { IconKey } from 'utils/iconMap'
 
 export default function Home() {
   
 
-  // const features =[
-  //     {
-  //       "icon": "feature1",
-  //       "title": "Ready to respond to your inquiries",
-  //       "description": "Automated response available 24/7 and a specialized team to serve you during business hours"
-  //     },
-  //     {
-  //       "icon": "feature2",
-  //       "title": "Vibrant Community",
-  //       "description": "Connect with fellow dreamers and doers who speak your language"
-  //     },
-  //     {
-  //       "icon": "feature3",
-  //       "title": "Expert Support",
-  //       "description": "Our team of experts is ready to answer your questions throughout working hours."
-  //     }
-  //   ]
+  const features =[
+      {
+        "icon": "feature1",
+        "title": "Ready to respond to your inquiries",
+        "description": "Automated response available 24/7 and a specialized team to serve you during business hours"
+      },
+      {
+        "icon": "feature2",
+        "title": "Vibrant Community",
+        "description": "Connect with fellow dreamers and doers who speak your language"
+      },
+      {
+        "icon": "feature3",
+        "title": "Expert Support",
+        "description": "Our team of experts is ready to answer your questions throughout working hours."
+      }
+    ]
 
 
   return (
@@ -36,7 +36,7 @@ export default function Home() {
        <Hero />
       <Partners />
       <Features title="Empowering Businesses of All Shapes and Sizes"/>  
-      {/* <StoreBuilder
+      <StoreBuilder
       title="VERSATILE STORE BUILDER"
       description="Launch your dream store online or manage your physical spot with ease. No coding nightmares, we promise."
       buttonText="Build Your Empire"
@@ -52,10 +52,10 @@ export default function Home() {
       backgroundColor="#dddddd69"
      
     />
-  <SuccessStories /> */}
-  {/* <Features2 />
-  <Features title="Empowering Businesses of All Shapes and Sizes" /> */}
-{/* <SupportSection
+  <SuccessStories />
+  <Features2 />
+  <Features title="Empowering Businesses of All Shapes and Sizes" />
+<SupportSection
   title="Here to Support Your Journey"
   subtitle="Whether you're just starting out or scaling up, our dedicated support team is here to help you every step of the way."
   features={features}
@@ -74,7 +74,7 @@ export default function Home() {
 
   paddingX="px-10 md:px-20"
   paddingY="pt-0 pb-20"
-/> */}
+/>
 
 
     </>
