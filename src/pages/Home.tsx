8b37523a57bec6bd7a8fd7788dbcab6c98ef5276
AHMED739268 <mo017886@gmail.com>
2025-08-29 17:15:05 +0300

    softwarehouse

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,34 @@
 import Hero from '../sections/Hero'
 import Partners from '../sections/Partners'
-import { useTranslation } from "react-i18next";
+
 import Features from '../pages/FeaturesPage'
 import Features2 from '../pages/FeaturesPage2'
 import StoreBuilder from '../sections/StoreBuilder'
 import StoreBuilderRight from '../sections/StoreBuilderRight'
 import SuccessStories from '../pages/SuccessStories'
-// import {SupportSection} from '../sections/SupportSection';
+import {SupportSection} from '../sections/SupportSection';
 import { IconKey } from 'utils/iconMap'
 
 export default function Home() {
-  //  const { t } = useTranslation();
+  
 
-  // const features = t("supportSection.features", {
-  //   returnObjects: true,
-  // }) as { icon: IconKey; title: string; description: string }[];
+  const features =[
+      {
+        "icon": "feature1",
+        "title": "Ready to respond to your inquiries",
+        "description": "Automated response available 24/7 and a specialized team to serve you during business hours"
+      },
+      {
+        "icon": "feature2",
+        "title": "Vibrant Community",
+        "description": "Connect with fellow dreamers and doers who speak your language"
+      },
+      {
+        "icon": "feature3",
+        "title": "Expert Support",
+        "description": "Our team of experts is ready to answer your questions throughout working hours."
+      }
+    ]
 
 
   return (
@@ -41,26 +55,26 @@ export default function Home() {
   <SuccessStories />
   <Features2 />
   <Features title="Empowering Businesses of All Shapes and Sizes" />
-{/* <SupportSection
-  title={t("supportSection.title")}
-  subtitle={t("supportSection.subtitle")}
+<SupportSection
+  title="Here to Support Your Journey"
+  subtitle="Whether you're just starting out or scaling up, our dedicated support team is here to help you every step of the way."
   features={features}
-  buttonText={t("supportSection.buttonText")}
+  buttonText="Contact Support"
   backgroundColor="#B368F5"
 
   paddingX="px-10 md:px-20"
   paddingY="pt-20 pb-5"
 />
 <SupportSection
-  title={t("supportSection.title")}
-  subtitle={t("supportSection.subtitle")}
+  title="Here to Support Your Journey"
+  subtitle="Whether you're just starting out or scaling up, our dedicated support team is here to help you every step of the way."
   features={features}
-  buttonText={t("supportSection.buttonText")}
+  buttonText="Contact Support"
   backgroundColor="#aeaeaeff"
 
   paddingX="px-10 md:px-20"
   paddingY="pt-0 pb-20"
-/> */}
+/>
 
 
     </>
